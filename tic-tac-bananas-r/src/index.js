a5d559a89125a7e3c4eb523950c49698aa67742b
s-c <48266366+s-c-@users.noreply.github.com>
2020-06-29 14:17:46 -0400

    changed functions port to 5020

--- tic-tac-bananas-r/src/index.js
+++ tic-tac-bananas-r/src/index.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   projectId: "tic-tac-bananas",
   storageBucket: "tic-tac-bananas.appspot.com",
   messagingSenderId: "480289553205",
-  appId: "1:480289553205:web:c9bd11ad551fec5cbd6a7c"
+  appId: "1:480289553205:web:4d25eef1ad733721bd6a7c"
 };
 
 firebase.initializeApp(firebaseConfig);
