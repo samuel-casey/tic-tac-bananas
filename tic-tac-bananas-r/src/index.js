693cd7011743d155f942d46896b0eaa73f7e8361
s-c <48266366+s-c-@users.noreply.github.com>
2020-06-29 17:51:18 -0400

    dev env and firebase configured

--- tic-tac-bananas-r/src/index.js
+++ tic-tac-bananas-r/src/index.js
@@ -5,17 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyDjipWGB6aW0H5x62zFJHuNhwrkzpRO5Sc",
   authDomain: "tic-tac-bananas.firebaseapp.com",
   databaseURL: "https://tic-tac-bananas.firebaseio.com",
   projectId: "tic-tac-bananas",
   storageBucket: "tic-tac-bananas.appspot.com",
   messagingSenderId: "480289553205",
-  appId: "1:480289553205:web:4d25eef1ad733721bd6a7c"
+  appId: "1:480289553205:web:c9bd11ad551fec5cbd6a7c"
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
