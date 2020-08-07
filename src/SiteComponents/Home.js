baffe537543426b39db9f0a6ea1fa5c705053f21
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-08-07 13:17:27 -0400

    styled header and wrote instructions

--- src/SiteComponents/Home.js
+++ src/SiteComponents/Home.js
@@ -2,5 +2,11 @@ import React from 'react'
 import { Navigation } from './Navigation'
 
 export const Home = () => {
-    return <h1><span role="img" aria-label="banana">🍌</span> tic-tac-bananas <span role="img" aria-label="banana">🍌</span></h1>
+    return (
+        <div id="header">
+            <h1><span role="img" aria-label="banana">🍌</span> tic-tac-bananas <span role="img" aria-label="banana">🍌</span></h1>
+            <div id="header-instructions">A potassium-filled spin on a classic game.<br></br>First to get three bananas in a row wins!<br></br>Select an option below for how you want to play:</div>
+        </div>
+    )
+
 }
